81607beaef669fa2eb65aad90552d16f856289f4
carolinamaciel <anna.maciel@conceptu.ind.br>
2020-05-01 22:46:26 -0300

    Initial commits

---
+++ saints/admin.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from saints.models import Saints
+from django.contrib import admin
+
+admin.site.register(Saints)
---
+++ saints/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+
+class Saints(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    country = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
